55a9131715850d395273e07158108ddfdbd8be43
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-12-01 08:58:44 +0530

    commit 8

--- p2.py
+++ p2.py
@@ -1,14 +1,6 @@
-f = open("demofile2.txt", "w")
-f.write("Now the file!1\n")
-f.write("Now the file!2\n")
-
-f = open("demofile2.txt", "a")
-f.write("Now the file has more content!\n")
-f.write("Now the file has more content2!\n")
-
-f.write("Now the file has more content33!\n")
-
+f = open("demofile.txt", "a")
+f.write("Now the file has more content!")
 f.close()
 #open and read the file after the appending:
-f = open("demofile2.txt", "r")
+f = open("demofile.txt", "r")
 print(f.read())
---
+++ p2_2.py
@@ -0,0 +1,14 @@
+f = open("demofile2.txt", "w")
+f.write("Now the file!1\n")
+f.write("Now the file!2\n")
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!\n")
+f.write("Now the file has more content2!\n")
+
+f.write("Now the file has more content33!\n")
+
+f.close()
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
